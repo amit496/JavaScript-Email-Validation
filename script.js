7499975667d780140888c436f4114a04062efc7e
Rays Coding <69256120+amit496@users.noreply.github.com>
2024-04-07 13:42:26 +0530

    Update => 04/07/2024

---
+++ script.js
@@ -0,0 +1,36 @@
+    
+    function validation()
+    {
+        // get the input box value
+        let inputVal = document.getElementById('email').value.trim();
+        // Regex value
+        let nameRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        // Conditions
+        if (inputVal === '')
+        {
+            document.getElementById('error').innerText = "Please enter the email.";
+            document.getElementById('errorRow').style.display = "block";
+            hideError();
+            return false;
+        }
+        else if (!nameRegex.test(inputVal))
+        {
+            document.getElementById('error').innerText = "Please enter a valid email.";
+            document.getElementById('errorRow').style.display = "block";
+            hideError();
+            return false;
+        }
+        else
+        {
+            document.getElementById('success').innerText = 'You Enter ' + inputVal;
+            document.getElementById('successRow').style.display = "block";
+            return true;
+        }
+    }
+    // Hide error message
+    function hideError()
+    {
+        setTimeout(function() {
+            document.getElementById('errorRow').style.display = "none";
+        }, 5000);
+    }
